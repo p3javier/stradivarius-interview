5d741052cb1cbae0c10bf2a96464b01b77b439f3
Javier Perez <javier@iul-inst.com>
2022-09-09 11:19:46 +0200

    first commit

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,9 @@
+import classes from "./Layout.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <main className={classes.main}>{children}</main>
+    </div>
+  );
+}
---
+++ src/pages/AllMeetupsPage.js
@@ -0,0 +1,16 @@
+import MeetupItem from "../components/meetups/MeetupItem";
+import classes from "./../components/meetups/MeetupList.module.css";
+
+export default function AllMeetupsPage() {
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <ul className={classes.list}>
+        <MeetupItem />
+        <MeetupItem />
+        <MeetupItem />
+        <MeetupItem />
+      </ul>
+    </section>
+  );
+}
